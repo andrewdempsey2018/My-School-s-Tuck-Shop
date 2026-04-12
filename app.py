42be3567a216ed8012116a9565ffe8f8bc3330ad
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2026-04-12 09:30:31 +0100

    add templates

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ def index():
     name = 'Clarence Bodiker'
     return render_template('index.html', title='Welcome', username=name)
 
+@app.route('/about')
+def about():
+    name = 'Clarence Bodiker'
+    return render_template('about.html', title='aoutere', username=name)
+
+@app.route('/all')
+def all():
+    name = 'asdf adf'
+    return render_template('all.html', title='alltitle', username=name)
+
 @app.route("/test")
 def hello_world():
     return "<p>Hello, school!</p>"
